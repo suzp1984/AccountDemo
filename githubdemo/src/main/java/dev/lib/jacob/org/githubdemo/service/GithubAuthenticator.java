64f5e0c4edb14c96ef33e2f5f96bef5d38d72c55
suzp1984 <suzp1984@gmail.com>
2015-09-15 10:59:10 +0800

    add GithubDemo

---
+++ githubdemo/src/main/java/dev/lib/jacob/org/githubdemo/service/GithubAuthenticator.java
@@ -0,0 +1,63 @@
+package dev.lib.jacob.org.githubdemo.service;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by moses on 9/15/15.
+ */
+public class GithubAuthenticator extends AbstractAccountAuthenticator {
+
+    private final Context mContext;
+
+    public GithubAuthenticator(Context context) {
+        super(context);
+
+        mContext = context;
+    }
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+        return null;
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType,
+            String authTokenType, String[] requiredFeatures, Bundle options)
+            throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account,
+            Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account,
+            String authTokenType, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public String getAuthTokenLabel(String authTokenType) {
+        return null;
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account,
+            String authTokenType, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account,
+            String[] features) throws NetworkErrorException {
+        return null;
+    }
+}
